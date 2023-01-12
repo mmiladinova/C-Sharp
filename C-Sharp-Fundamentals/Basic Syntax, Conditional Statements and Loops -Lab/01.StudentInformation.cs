4cbb99afebfd0ee188312b39dadb7c14fce9e10a
mmiladinova <mmiladinova@users.noreply.github.com>
2023-01-12 21:17:40 +0200

    Create 01.StudentInformation.cs

---
+++ C-Sharp-Fundamentals/Basic Syntax, Conditional Statements and Loops -Lab/01.StudentInformation.cs
@@ -0,0 +1,5 @@
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+double avrgGrade = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {name}, Age: {age}, Grade: {avrgGrade:F2}");
