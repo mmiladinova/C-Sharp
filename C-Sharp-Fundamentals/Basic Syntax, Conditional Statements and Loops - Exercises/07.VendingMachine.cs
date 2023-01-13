57c9fa1b64ac2faa196d70c516447f0817d670e3
mmiladinova <mmiladinova@users.noreply.github.com>
2023-01-13 14:49:02 +0200

    Add files via upload

---
+++ C-Sharp-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercises/07.VendingMachine.cs
@@ -0,0 +1,112 @@
+﻿string input = Console.ReadLine();
+
+double totalMoney = 0;
+double price = 0;
+
+while (true)
+{
+    if (input == "Start")
+    {
+        break;
+    }
+    else
+    {
+        double inputSum = double.Parse(input);
+        if (inputSum == 0.1 || inputSum == 0.2 || inputSum == 0.5 || inputSum == 1 || inputSum == 2)
+        {
+            totalMoney += inputSum;
+        }
+        else
+        {
+            Console.WriteLine($"Cannot accept {inputSum}");
+        }
+    }
+    input = Console.ReadLine();
+}
+
+while (true)
+{
+    if (input == "Start")
+    {
+        input = Console.ReadLine();
+        continue;
+    }
+    if (input == "End")
+    {
+        Console.WriteLine($"Change: {totalMoney:F2}");
+        break;
+    }
+    else
+    {
+        if (input == "Nuts")
+        {
+            price = 2.0;
+            if (totalMoney - price >= 0)
+            {
+                totalMoney -= price;
+                Console.WriteLine($"Purchased {input.ToLower()}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough money");
+            }
+        }
+        else if (input == "Water")
+        {
+            price = 0.7;
+            if (totalMoney - price >= 0)
+            {
+                totalMoney -= price;
+                Console.WriteLine($"Purchased {input.ToLower()}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough money");
+            }
+        }
+        else if (input == "Crisps")
+        {
+            price = 1.5;
+            if (totalMoney - price >= 0)
+            {
+                totalMoney -= price;
+                Console.WriteLine($"Purchased {input.ToLower()}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough money");
+            }
+        }
+        else if (input == "Soda")
+        {
+            price = 0.8;
+            if (totalMoney - price >= 0)
+            {
+                totalMoney -= price;
+                Console.WriteLine($"Purchased {input.ToLower()}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough money");
+            }
+        }
+        else if (input == "Coke")
+        {
+            price = 1.0;
+            if (totalMoney - price >= 0)
+            {
+                totalMoney -= price;
+                Console.WriteLine($"Purchased {input.ToLower()}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough money");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid product");
+        }
+    }
+    input = Console.ReadLine();
+}
---
+++ C-Sharp-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercises/10.RageExpenses.cs
@@ -0,0 +1,19 @@
+﻿int lostGames = int.Parse(Console.ReadLine());
+double headsetPrice = double.Parse(Console.ReadLine());
+double mousePrice = double.Parse(Console.ReadLine());
+double keyboardPrice = double.Parse(Console.ReadLine());
+double displayPrice = double.Parse(Console.ReadLine());
+
+int trashedHeadsets = lostGames / 2;
+int trashedMouses = lostGames / 3;
+int trashedKeyboards = lostGames / 6;
+int trashedDisplays = trashedKeyboards / 2;
+
+double totalPriseForTrashes = 0.0;
+
+totalPriseForTrashes = (trashedHeadsets * headsetPrice) + (trashedMouses * mousePrice) + (trashedKeyboards * keyboardPrice) + (trashedDisplays * displayPrice);
+
+Console.WriteLine($"Rage expenses: {totalPriseForTrashes:F2} lv.");
+
+
+
