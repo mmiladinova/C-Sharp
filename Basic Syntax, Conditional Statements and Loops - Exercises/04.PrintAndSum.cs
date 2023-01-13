42ab9ed5aa898afbd9dee22b224763d55a90db01
mmiladinova <mmiladinova@users.noreply.github.com>
2023-01-13 14:44:39 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercises/04.PrintAndSum.cs
@@ -0,0 +1,13 @@
+﻿int startNum = int.Parse(Console.ReadLine());
+int endNum = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (startNum <= endNum)
+{
+    Console.Write(startNum + " ");
+    sum += startNum;
+    startNum++;
+}
+Console.WriteLine();
+Console.WriteLine($"Sum: {sum}");
---
+++ Basic Syntax, Conditional Statements and Loops - Exercises/11.Orders.cs
@@ -0,0 +1,17 @@
+﻿int receivedOrders = int.Parse(Console.ReadLine());
+
+double totalPrice = 0;
+
+for (int order = 1; order <= receivedOrders; order++)
+{
+    double pricePerCapsule = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsulesCount = int.Parse(Console.ReadLine());
+
+    double price = (days * capsulesCount) * pricePerCapsule;
+    totalPrice += price;
+
+    Console.WriteLine($"The price for the coffee is: ${price:F2}");
+}
+
+Console.WriteLine($"Total: ${totalPrice:F2}");
